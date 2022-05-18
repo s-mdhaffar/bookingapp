8b33365e17207d25cbb03cd09771d898711ed51d
s-mdhaffar <s.mdhaffar@gmail.com>
2022-05-18 11:16:09 +0100

    Hotel model

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,18 @@
 import express from 'express';
+import Hotel from '../models/Hotel.js';
 
 const router = express.Router();
+
+//Create
+
+router.post('/', async (req, res) => {
+	const newHotel = new Hotel(req.body);
+	try {
+		const savedHotel = await newHotel.save();
+		res.status(200).json(savedHotel);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
 export default router;
