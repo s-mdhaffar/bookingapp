deb8e1516cbfbd8a4c55a4e719e1bb06c576ec32
s-mdhaffar <s.mdhaffar@gmail.com>
2022-05-20 16:30:53 +0100

    Hotels route

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -15,4 +15,48 @@ router.post('/', async (req, res) => {
 	}
 });
 
+//Update
+
+router.put('/:id', async (req, res) => {
+	try {
+		const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+		res.status(200).json(updatedHotel);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+//Get
+
+router.get('/:id', async (req, res) => {
+	try {
+		const getHotel = await Hotel.findById(req.params.id);
+		res.status(200).json(getHotel);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+//Get All
+
+router.get('/', async (req, res) => {
+	try {
+		const Hotels = await Hotel.find();
+		res.status(200).json(Hotels);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+//Delete
+
+router.delete('/:id', async (req, res) => {
+	try {
+		await Hotel.findByIdAndDelete(req.params.id);
+		res.status(200).json('Hotel Deleted');
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
 export default router;
