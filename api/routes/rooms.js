eb2eb3da0df0413cb08255165ea2f66d88bf8bd5
s-mdhaffar <s.mdhaffar@gmail.com>
2022-05-23 19:32:21 +0100

    Rooms

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,27 @@
 import express from 'express';
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/room.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
+
+//Create
+
+router.post('/:hotelid', verifyAdmin, createRoom);
+
+//Update
+
+router.put('/:id', verifyAdmin, updateRoom);
+
+//Get
+
+router.get('/:id', getRoom);
+
+//Get All
+
+router.get('/', getRooms);
+
+//Delete
+
+router.delete('/:id/:hotelid', verifyAdmin, deleteRoom);
 
 export default router;
